882e160c433290df7492f404891fe4f734e27e66
bocaben <bocaben@gmail.com>
2017-03-28 17:31:14 +0300

    from lesson 2. hc-06 bluetooth

---
+++ bluetooth_echo_uppercase/bluetooth_echo_uppercase.ino
@@ -0,0 +1,28 @@
+String message; //string that stores the incoming message
+char ch;
+
+void setup()
+{
+  Serial.begin(9600); //set baud rate
+}
+
+void loop()
+{
+  while(Serial.available())
+  {//while there is data available on the serial monitor
+    ch = char(Serial.read());//store string from serial command
+    if (ch>=97)
+      ch -= ('a'-'A');
+    message+=ch;
+  }
+  if(!Serial.available())
+  {
+    if(message!="")
+    {//if data is available
+      Serial.println(message); //show the data
+      message=""; //clear the data
+    }
+  }
+  delay(200); //delay
+}
+    
